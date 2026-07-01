6c2fca1f01fb31193db8734c5b17f33f34c9b6c9
Me <hermelin.lois06@gmail.com>
2026-07-01 16:35:14 +0200

    update makefile

--- pmns_exec/codes/interfaces/measurement_utils_interface.h
+++ pmns_exec/codes/interfaces/measurement_utils_interface.h
@@ -12,10 +12,13 @@ extern "C" {
 #endif
 
 void quicksort(uint64_t* t, int n);
+
 uint64_t *quartiles(uint64_t *tab, uint64_t size);
 
 uint64_t cpucyclesStart(void);
+
 uint64_t cpucyclesStop(void);
+
 uint64_t cycles_diff(uint64_t start, uint64_t stop);
 
 #ifdef __cplusplus
